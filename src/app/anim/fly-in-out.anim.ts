ba70dd3953dccbb8381630bbea811176bee94e6d
wpcfan <peng.wang@outlook.com>
2017-05-09 16:17:38 +0800

    add more style

--- src/app/anim/fly-in-out.anim.ts
+++ src/app/anim/fly-in-out.anim.ts
@@ -4,7 +4,7 @@ export const flyInOutAnim = trigger('flyInOut', [
   state('in', style({transform: 'translateX(0)'})),
   transition('void => *', [// `void => *` 可以写成 `:enter`
     style({transform: 'translateX(100%)'}),
-    animate(300)
+    animate('300ms')
   ]),
   transition('* => void', [// `void => *` 可以写成 `:leave`
     animate(300, style({transform: 'translateX(-100%)'}))
