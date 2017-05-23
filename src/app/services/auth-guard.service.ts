1e9f9b1852d9c5d8a018163f8bb073cfd4d67ce3
wpcfan <peng.wang@outlook.com>
2017-05-23 22:34:53 +0800

    update guard

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -7,35 +7,20 @@ import {
 } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/takeUntil';
 import 'rxjs/add/operator/defaultIfEmpty';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Store } from '@ngrx/store';
 import { go } from "@ngrx/router-store";
-import { Actions } from "@ngrx/effects";
 import * as fromRoot from '../reducers';
-import * as actions from '../actions/auth.action';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
-  private _authSubject: BehaviorSubject<boolean>= new BehaviorSubject<boolean>(false);
-  /**
+   /**
    * 构造函数用于注入服务的依赖以及进行必要的初始化
    * 
    * @param router 路由注入，用于导航处理
    * @param store$ redux store注入，用于状态管理
    */
-  constructor(
-    private actions$: Actions,
-    private store$: Store<fromRoot.State>) { 
-      this.store$
-        .select(s => s.auth)
-        .subscribe(auth => {
-          const result = (auth.user !== undefined);
-          this._authSubject.next(result);
-          if(!result) this.store$.dispatch(go(['/login']));
-        });
-    }
+  constructor(private store$: Store<fromRoot.State>) { }
 
   /**
    * 用于判断是否可以激活该路由
@@ -47,6 +32,13 @@ export class AuthGuardService implements CanActivate {
   }
 
   checkAuth(): Observable<boolean>{
-    return this._authSubject.asObservable();
+    return this.store$
+        .select(s => s.auth)
+        .map(auth => {
+          const result = auth.token !== undefined && auth.token !== null;
+          if(!result) this.store$.dispatch(go(['/login']));
+          return result;
+        })
+        .defaultIfEmpty(false);
   }
 }
