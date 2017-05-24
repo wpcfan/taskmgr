deee5c2499d643f5fa8f3d6c9fa48bee1de0d7e9
wpcfan <peng.wang@outlook.com>
2017-05-25 01:17:23 +0800

    make e2e work again, test cases to be added soon

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('taskmgr App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toContain('企业协作平台');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,6 @@ export class TaskMgrPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-root md-sidenav-container')).getText();
   }
 }
