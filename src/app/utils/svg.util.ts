ee4ea37cbe598a0eab02746d1891c54e002399e6
wpcfan <peng.wang@outlook.com>
2017-06-04 21:58:54 +0800

    revise the svgutil

--- src/app/utils/svg.util.ts
+++ src/app/utils/svg.util.ts
@@ -1,22 +1,23 @@
 import { MdIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
 
-export const loadSvgResources = (iconRegistry: MdIconRegistry, sanitizer: DomSanitizer) => {
-  const avatarName = 'avatars';
-  const avatarsSafeUrl = sanitizer.bypassSecurityTrustResourceUrl('assets/img/avatar/avatars.svg');
-  iconRegistry.addSvgIconSetInNamespace(avatarName, avatarsSafeUrl);
-  iconRegistry.addSvgIcon('project', sanitizer.bypassSecurityTrustResourceUrl('assets/img/sidebar/project.svg'));
-  iconRegistry.addSvgIcon('projects', sanitizer.bypassSecurityTrustResourceUrl('assets/img/sidebar/projects.svg'));
-  iconRegistry.addSvgIcon('month', sanitizer.bypassSecurityTrustResourceUrl('assets/img/sidebar/month.svg'));
-  iconRegistry.addSvgIcon('week', sanitizer.bypassSecurityTrustResourceUrl('assets/img/sidebar/week.svg'));
-  iconRegistry.addSvgIcon('day', sanitizer.bypassSecurityTrustResourceUrl('assets/img/sidebar/day.svg'));
-  iconRegistry.addSvgIcon('move',sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/move.svg'));
+export const loadSvgResources = (ir: MdIconRegistry, ds: DomSanitizer) => {
+  const imgDir = 'assets/img';
+  const avatarDir = `${imgDir}/avatar`;
+  const sidebarDir = `${imgDir}/sidebar`;
+  const iconDir = `${imgDir}/icons`;
+  const dayDir = `${imgDir}/days`;
+  ir.addSvgIconSetInNamespace('avatars', ds.bypassSecurityTrustResourceUrl(`${avatarDir}/avatars.svg`));
+  ir.addSvgIcon('project', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/project.svg`));
+  ir.addSvgIcon('projects', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/projects.svg`));
+  ir.addSvgIcon('month', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/month.svg`));
+  ir.addSvgIcon('week', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/week.svg`));
+  ir.addSvgIcon('day', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/day.svg`));
+  ir.addSvgIcon('move',ds.bypassSecurityTrustResourceUrl(`${iconDir}/move.svg`));
   const days = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
     11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
   ];
-  days.forEach(day => {
-    iconRegistry.addSvgIcon(`day${day}`,sanitizer.bypassSecurityTrustResourceUrl(`assets/img/days/day${day}.svg`));
-  });
+  days.forEach(day => ir.addSvgIcon(`day${day}`,ds.bypassSecurityTrustResourceUrl(`${dayDir}/day${day}.svg`)));
 };
