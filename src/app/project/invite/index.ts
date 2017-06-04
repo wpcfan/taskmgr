c498f9cb54774a631e45bf01aa1c24f312954958
wpcfan <peng.wang@outlook.com>
2017-06-05 00:05:54 +0800

    fix image list issue

--- src/app/project/invite/index.ts
+++ src/app/project/invite/index.ts
@@ -8,13 +8,10 @@ import {User} from '../../domain';
     <md-card>
       <h2 md-dialog-title>{{dialogTitle}}</h2>
       <form class="full-width" #f="ngForm" (ngSubmit)="onSubmit($event, f)">
-        <app-chips-list [items]="members" [label]="'邀请成员'" name="members" [(ngModel)]="members"></app-chips-list>
+        <app-chips-list [items]="members" [label]="'邀请成员'" name="members" [(ngModel)]="members">
+        </app-chips-list>
         <div md-dialog-actions>
-          <button
-            md-raised-button
-            color="primary"
-            type="submit"
-            [disabled]="!f.valid">
+          <button md-raised-button color="primary" type="submit" [disabled]="!f.valid">
             保存
           </button>
           <button md-dialog-close md-raised-button type="button">关闭</button>
