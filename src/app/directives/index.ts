24f6e6a15a460f8a35935b98dc039a3751710590
wpcfan <peng.wang@outlook.com>
2017-06-05 14:00:58 +0800

    remove common module from directives module

--- src/app/directives/index.ts
+++ src/app/directives/index.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { DragDirective } from './drag-drop/drag.directive';
 import { DropDirective } from './drag-drop/drop.directive';
 import { DragDropService } from './drag-drop.service';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
   providers: [
     DragDropService
   ],
