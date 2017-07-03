d22d4d8eb31f0bcb0d79d8a9a29d8af9dada6688
wpcfan <peng.wang@outlook.com>
2017-07-03 18:52:58 +0800

    change item animation timing

--- src/app/anim/item.anim.ts
+++ src/app/anim/item.anim.ts
@@ -3,7 +3,7 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
 export const itemAnim = trigger('item', [
   state('in', style({'border-left-width': '3px'})),
   state('out', style({'border-left-width': '8px'})),
-  transition('out => hover', animate('100ms ease-in')),
-  transition('hover => out', animate('100ms ease-out'))
+  transition('out => hover', animate('500ms ease-in')),
+  transition('hover => out', animate('500ms ease-out'))
 ]);
 
