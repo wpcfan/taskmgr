723afc94f573ee2d375b34b2edc175752ba492c8
wpcfan <peng.wang@outlook.com>
2017-07-07 22:42:06 +0800

    quicktask can prevent empty string

--- src/app/task/quick-task/index.ts
+++ src/app/task/quick-task/index.ts
@@ -7,13 +7,16 @@ import { Component, Output, EventEmitter, HostListener } from '@angular/core';
 })
 export class QuickTaskComponent {
 
-  desc = '';
+  desc: string;
   @Output() quickTask = new EventEmitter<string>();
 
   constructor() { }
 
   @HostListener('keyup.enter')
   sendQuickTask() {
+    if (!this.desc || this.desc.length === 0 || !this.desc.trim()) {
+      return;
+    }
     this.quickTask.emit(this.desc);
     this.desc = '';
   }
