d0442673175ac59b488cb93406e8d56b292a18e6
wpcfan <peng.wang@outlook.com>
2017-07-27 12:47:04 +0800

    fix ie 11 layout issue which is raised by animation style

--- src/app/anim/router.anim.ts
+++ src/app/anim/router.anim.ts
@@ -1,8 +1,8 @@
 import {animate, state, style, transition, trigger, group, AnimationTriggerMetadata} from '@angular/animations';
 
 export const slideToRight = trigger('routeAnim', [
-  state('void', style({'position': 'fixed', 'width': '100%', 'height': '80%'})),
-  state('*', style({'position': 'fixed', 'width': '100%', 'height': '80%'})),
+  state('void', style({'width': '100%', 'height': '80%'})),
+  state('*', style({'width': '100%', 'height': '80%'})),
   transition(':enter', [
     style({transform: 'translateX(-100%)', opacity: 0}),
     group([
