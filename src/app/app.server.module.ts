0d5270d3810530b932dce59952151cc18141a2a0
wpcfan <peng.wang@outlook.com>
2017-08-08 13:08:12 +0800

    add comments to app.server.module

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -4,6 +4,9 @@ import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
+/**
+ * Used for server rendering
+ */
 @NgModule({
   imports: [
     AppModule,
