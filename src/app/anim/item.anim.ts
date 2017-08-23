bfe2a178dad43be0dd5026f28bfe1b9a19ea1261
wpcfan <peng.wang@outlook.com>
2017-08-23 13:35:59 +0800

    fix animation bug

--- src/app/anim/item.anim.ts
+++ src/app/anim/item.anim.ts
@@ -3,7 +3,7 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
 export const itemAnim = trigger('item', [
   state('in', style({'border-left-width': '3px'})),
   state('out', style({'border-left-width': '8px'})),
-  transition('out => hover', animate('500ms ease-in')),
-  transition('hover => out', animate('500ms ease-out'))
+  transition('out => in', animate('500ms ease-in')),
+  transition('in => out', animate('500ms ease-out'))
 ]);
 
