c7fcd346b76e1c9ae96aaafcd4ac553115f30a70
wpcfan <peng.wang@outlook.com>
2017-09-07 22:57:10 +0800

    remove unneccesary imports

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+import {OverlayContainer} from '@angular/material';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  private _dark = false;
+
+  constructor(private oc: OverlayContainer) {
+  }
+
+  get dark() {
+    return this._dark;
+  }
+
+  switchDarkTheme(dark: boolean) {
+    this._dark = dark;
+    this.oc.themeClass = dark ? 'myapp-dark-theme' : null;
+  }
+}
