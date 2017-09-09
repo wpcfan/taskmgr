c0fdf328b8202f1af3dfcfd6430ab923dcc9fd3c
wpcfan <peng.wang@outlook.com>
2017-09-10 02:13:19 +0800

    split rx imports to src/rxjs.import.ts

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -18,33 +18,7 @@ import {PageNotFoundComponent} from './page-not-found';
 import {loadSvgResources} from '../utils/svg.util';
 import {DateFnsAdapter} from '../shared/adapters/date-fns-adapter';
 import {DatepickerI18n} from '../shared/adapters/datepicker-i18n';
-import { MD_FNS_DATE_FORMATS } from '../shared/adapters/date-formats';
-
-import '../utils/debug.util';
-import 'rxjs/add/observable/from';
-import 'rxjs/add/observable/fromEvent';
-import 'rxjs/add/observable/concat';
-import 'rxjs/add/observable/zip';
-import 'rxjs/add/observable/range';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/observable/merge';
-import 'rxjs/add/observable/combineLatest';
-import 'rxjs/add/operator/startWith';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mapTo';
-import 'rxjs/add/operator/pluck';
-import 'rxjs/add/operator/defaultIfEmpty';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/reduce';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/withLatestFrom';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/take';
-import 'rxjs/add/operator/count';
-import 'rxjs/add/operator/do';
+import {MD_FNS_DATE_FORMATS} from '../shared/adapters/date-formats';
 
 @NgModule({
   imports: [
