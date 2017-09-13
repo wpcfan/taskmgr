6228a712c8fc0bb52935f1409470b1ee961fc3df
Peng Wang <peng.wang@outlook.com>
2017-09-13 23:23:25 +0800

    restructure projects, shared, and directives

--- src/app/project/project-routing.module.ts
+++ src/app/project/project-routing.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {ProjectListComponent} from './project-list';
+import {ProjectListComponent} from './containers/project-list';
 
 const routes: Routes = [
   {path: '', component: ProjectListComponent}
