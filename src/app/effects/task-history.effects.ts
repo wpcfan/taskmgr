66706e792acf95cc4a1a4010c4ce9d5a6e450e3d
zhangzhihai <zhangzhihai@neusoft.com>
2017-09-15 15:15:51 +0800

    Added By ZhangZhihai For Create Task History
    Signed-off-by: zhangzhihai <zhangzhihai@neusoft.com>

--- src/app/effects/task-history.effects.ts
+++ src/app/effects/task-history.effects.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
-import { Action } from '@ngrx/store';
+import { Action, Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { TaskHistoryService } from '../services';
 import * as actions from '../actions/task-history.action';
-import { TaskHistory } from '../domain';
+import * as fromRoot from '../reducers';
+import * as History from '../domain/history';
+import { User, Task, TaskHistory } from '../domain';
 
 @Injectable()
 export class TaskHistoryEffects {
 
-  constructor(private actions$: Actions, private services$: TaskHistoryService) {
+  constructor(private actions$: Actions,
+    private services$: TaskHistoryService,
+    private store$: Store<fromRoot.State>) {
   }
 
   @Effect()
@@ -23,12 +27,36 @@ export class TaskHistoryEffects {
       .catch(err => of(new actions.LoadHistoryFailAction(JSON.stringify(err))))
     );
 
-  // @Effect()
-  // addTaskHistory$: Observable<Action> = this.actions$
-  //   .ofType<actions.CreateTaskAction>(actions.CREATE_TASK)
-  //   .map(action => action.payload)
-  //   .switchMap((history: TaskHistory) => this.services$.addTaskHistory(history)
-  //     .map((history: TaskHistory) => new actions.CreateTaskSuccessAction(history))
-  //     .catch(err => of(new actions.CreateTaskFailAction(JSON.stringify(err))))
-  //   );
+  @Effect()
+  addCreateTaskHistory$: Observable<Action> = this.actions$
+    .ofType<actions.CreateTaskAction>(actions.CREATE_TASK)
+    .map(action => action.payload)
+    .withLatestFrom(this.store$.select(fromRoot.getAuthUser))
+    .switchMap(([task, user]: [Task, User]) => {
+      const operator: User = {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        avatar: user.avatar,
+      }
+
+      const operation: History.CreateTaskOperation = {
+        type: History.CREATE_TASK,
+        payload: task.desc
+      };
+
+      const taskHistory: TaskHistory = {
+        taskId: <string>task.id,
+        operator: operator,
+        operation: operation,
+        date: new Date(),
+      }
+
+      console.log('<<Create Task History>>', JSON.stringify(taskHistory));
+
+      return this.services$
+        .addTaskHistory(taskHistory)
+        .map((history: TaskHistory) => new actions.CreateTaskSuccessAction(history))
+        .catch(err => of(new actions.CreateTaskFailAction(JSON.stringify(err))))
+    });
 }
