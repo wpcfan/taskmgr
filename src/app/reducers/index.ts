70baa793a6a46c04e4c6210467b2275e41535dee
wpcfan <peng.wang@outlook.com>
2017-09-20 14:30:19 +0800

    update project and tasklist using ngrx/entity

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -138,6 +138,20 @@ export const {
   selectTotal: getUserTotal
 } = fromUsers.adapter.getSelectors(getUsersState);
 
+export const {
+  selectIds: getProjectIds,
+  selectEntities: getProjectEntities,
+  selectAll: getProjects,
+  selectTotal: getProjectTotal
+} = fromProjects.adapter.getSelectors(getProjectsState);
+
+export const {
+  selectIds: getTaskListIds,
+  selectEntities: getTaskListEntities,
+  selectAll: getTaskLists,
+  selectTotal: getTaskListTotal
+} = fromTaskLists.adapter.getSelectors(getTaskListsState);
+
 const getSelectedProjectId = createSelector(getProjectsState, fromProjects.getSelectedId);
 
 const getTasksWithOwner = createSelector(getTasks, getUserEntities, (tasks, entities) => tasks.map(task =>
