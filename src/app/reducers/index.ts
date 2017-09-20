f5341bc14722ccce212fb1d0c5797cee20fdd827
wpcfan <peng.wang@outlook.com>
2017-09-20 15:33:52 +0800

    update auth and quote reducer using featureselector

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -43,13 +43,14 @@ import { initialState } from './user.reducer';
 
 import { TaskListVM } from '../vm';
 import { RouterStateUrl } from '../utils/router.util';
+import { Quote } from '../domain/quote';
 /**
  * 正如我们的 reducer 像数据库中的表一样，我们的顶层 state 也包含各个子 reducer 的 state
  * 并且使用一个 key 来标识各个子 state
  */
 export interface State {
   auth: Auth;
-  quote: fromQuote.State;
+  quote: Quote;
   projects: fromProjects.State;
   taskLists: fromTaskLists.State;
   tasks: fromTasks.State;
@@ -94,12 +95,12 @@ export const metaReducers: MetaReducer<State>[] = !environment.production
     ]
   : [storeStateGuard];
 
-export const getAuthState = (state: State) => state.auth;
-export const getQuoteState = (state: State) => state.quote;
+
 export const getTasksState = (state: State) => state.tasks;
 export const getUserState = (state: State) => state.users;
 
-export const getQuote = createSelector(getQuoteState, fromQuote.getQuote);
+export const getAuthState = createFeatureSelector<Auth>('auth');
+export const getQuoteState = createFeatureSelector<Quote>('quote');
 export const getProjectsState = createFeatureSelector<fromProjects.State>('projects');
 export const getTaskListsState = createFeatureSelector<fromTaskLists.State>('taskLists');
 export const getTasks = createSelector(getTasksState, fromTasks.getTasks);
@@ -158,7 +159,7 @@ export const getUserTasks = createSelector(getAuthUser, getTasks, (user, tasks) 
     StoreModule.forRoot(reducers, {metaReducers: metaReducers }),
     StoreRouterConnectingModule,
     // DevTool 需要在 StoreModule 之后导入
-    // !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 50 }) : []
+    !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 50 }) : []
   ]
 })
 export class AppStoreModule {
