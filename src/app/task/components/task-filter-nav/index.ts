b348f19dcabe94d8fa15c136ff79d4312f0e09f1
zhangzhihai <zhangzhihai@neusoft.com>
2017-10-10 17:22:27 +0800

    Added By ZhangZhihai For create task-filter-nav component
    Signed-off-by: zhangzhihai <zhangzhihai@neusoft.com>

---
+++ src/app/task/components/task-filter-nav/index.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-filter-nav',
+  templateUrl: './task-filter-nav.component.html',
+  styleUrls: ['./task-filter-nav.component.scss']
+})
+export class TaskFilterNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
