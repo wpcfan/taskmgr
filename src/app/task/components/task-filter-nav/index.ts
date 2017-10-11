32b2b26e08da8bc823792f450e0e0be075e7061d
zhangzhihai <zhangzhihai@neusoft.com>
2017-10-11 15:17:17 +0800

    Added By ZhangZhihai For task-filter-nav header
    Signed-off-by: zhangzhihai <zhangzhihai@neusoft.com>

--- src/app/task/components/task-filter-nav/index.ts
+++ src/app/task/components/task-filter-nav/index.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-task-filter-nav',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskFilterNavComponent implements OnInit {
 
+  @Output() closeClicked = new EventEmitter<void>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onCloseClicked(ev: Event) {
+    ev.preventDefault();
+    this.closeClicked.emit();
   }
 
 }
