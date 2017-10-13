986cb90e0d6f48dee5ba963cab801712aca43f4e
wpcfan <peng.wang@outlook.com>
2017-10-13 20:17:57 +0800

    refactor the auth service to use the backend

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,10 +12,6 @@ export class AuthService {
     'Content-Type': 'application/json'
   });
 
-  private token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' +
-    '.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9' +
-    '.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ';
-
   /**
    * 构造函数用于注入服务的依赖以及进行必要的初始化
    *
@@ -32,18 +28,8 @@ export class AuthService {
    * @param user 用户信息，id 属性会被忽略，因为服务器端会创建新的 id
    */
   register(user: User): Observable<Auth> {
-    const params = new HttpParams()
-      .set('email', user.email);
-    const uri = `${this.config.uri}/users`;
-    return this.http
-      .get(uri, {params})
-      .switchMap(res => {
-        if ((<User[]>res).length > 0) {
-          return Observable.throw('username existed');
-        }
-        return this.http.post(uri, JSON.stringify(user), {headers: this.headers})
-          .map(r => ({token: this.token, user: <User>r}));
-      });
+    const uri = `${this.config.uri}/auth/register`;
+    return this.http.post<Auth>(uri, JSON.stringify(user), {headers: this.headers});
   }
 
   /**
@@ -53,21 +39,8 @@ export class AuthService {
    * @param password 密码（明文），服务器会进行加密处理
    */
   login(email: string, password: string): Observable<Auth> {
-    const uri = `${this.config.uri}/users`;
-    const params = new HttpParams()
-      .set('email', email)
-      .set('password', password);
+    const uri = `${this.config.uri}/auth`;
     return this.http
-      .get(uri, {params})
-      .map(res => {
-        const users = <User[]>res;
-        if (users.length === 0) {
-          throw(new Error('Username or password incorrect'));
-        }
-        return {
-          token: this.token,
-          user: users[0]
-        };
-      });
+      .post<Auth>(uri, JSON.stringify({'username': email, 'password': password}), {headers: this.headers});
   }
 }
