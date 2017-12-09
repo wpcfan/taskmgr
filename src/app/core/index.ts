83d52094c0f1c7fbbd8f08ed1831c5504df27bba
wpcfan <peng.wang@outlook.com>
2017-12-10 00:56:37 +0800

    update to ng 5

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -2,7 +2,8 @@ import {NgModule, Optional, SkipSelf, LOCALE_ID} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {HttpModule} from '@angular/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatIconRegistry, DateAdapter, MAT_DATE_FORMATS, MatDatepickerIntl, MATERIAL_COMPATIBILITY_MODE} from '@angular/material';
+import {MatIconRegistry, DateAdapter, MAT_DATE_FORMATS, MatDatepickerIntl} from '@angular/material';
+import {MomentDateAdapter} from '@angular/material-moment-adapter';
 import {DomSanitizer} from '@angular/platform-browser';
 import {RouterStateSerializer} from '@ngrx/router-store';
 
@@ -19,7 +20,6 @@ import {PageNotFoundComponent} from './containers/page-not-found';
 import {AppComponent} from './containers/app';
 
 import {loadSvgResources} from '../utils/svg.util';
-import {DateFnsAdapter} from '../shared/adapters/date-fns-adapter';
 import {DatepickerI18n} from '../shared/adapters/datepicker-i18n';
 import {MD_FNS_DATE_FORMATS} from '../shared/adapters/date-formats';
 import {CustomRouterStateSerializer} from '../utils/router.util';
@@ -42,7 +42,7 @@ import '../utils/debug.util';
   providers: [
     {provide: 'BASE_CONFIG', useValue: { uri: 'http://localhost:3002'}},
     {provide: LOCALE_ID, useValue: 'zh-CN'},
-    {provide: DateAdapter, useClass: DateFnsAdapter},
+    {provide: DateAdapter, useClass: MomentDateAdapter},
     {provide: MAT_DATE_FORMATS, useValue: MD_FNS_DATE_FORMATS},
     {provide: MatDatepickerIntl, useClass: DatepickerI18n},
     /**
@@ -51,7 +51,6 @@ import '../utils/debug.util';
      * by `@ngrx/router-store` to include only the desired pieces of the snapshot.
      */
     { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer },
-    { provide: MATERIAL_COMPATIBILITY_MODE, useValue: true }
   ],
   declarations: [
     HeaderComponent,
